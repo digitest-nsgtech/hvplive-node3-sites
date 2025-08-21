2034a04ab373b0eb462b6104208a141b3d28c8ad
Raul Anso MBook <raulanso@gmail.com>
2025-08-21 15:43:50 +0300

    teine sats

---
+++ home.sillamaegymn.edu.ee/settings.php
@@ -0,0 +1,71 @@
+<?php
+
+
+if (!empty($_SERVER['HTTP_X_FORWARDED_PROTO'])
+  && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https'
+) {
+  $_SERVER['HTTPS'] = 'on';
+}
+
+// And enable Drupal’s built-in reverse-proxy trust:
+$settings['reverse_proxy'] = TRUE;
+// List the CIDR(s) or IP(s) of your ingress/load-balancer.
+$settings['reverse_proxy_addresses'] = [
+  '10.42.0.0/16',  // all Pod IPs (including your Ingress)
+  '127.0.0.1',     // local‐CLI testing if you ever curl from inside the box
+];
+// Trust all the X-Forwarded-* headers:
+$settings['reverse_proxy_trusted_headers'] = 31;
+
+
+$settings['config_sync_directory'] = $app_root . '/config/sync';
+
+
+// —————————————————————————————————————————————
+// Trusted host patterns must come first in settings.php
+// —————————————————————————————————————————————
+$settings['trusted_host_patterns'] = [
+  // Both with- and without- “www”
+  '^www\.home\.sillamaegymn\.edu\.ee$',
+  '^home\.sillamaegymn\.edu\.ee$',
+];
+
+$base_url = 'https://home.sillamaegymn.edu.ee';
+
+$settings['hash_salt']  = file_get_contents(__DIR__ . '/salt.txt');
+
+$settings['update_free_access'] = FALSE;
+$settings['file_public_path'] = 'sites/home.sillamaegymn.edu.ee/files';
+$settings['file_private_path'] = 'sites/home.sillamaegymn.edu.ee/privatefiles';
+$settings['file_temp_path'] = 'sites/home.sillamaegymn.edu.ee/tmp';
+
+/**
+ * OVERRIDES
+ */
+$settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
+
+$settings['file_scan_ignore_directories'] = [
+  'node_modules',
+  'bower_components',
+];
+$settings['entity_update_batch_size'] = 50;
+$settings['entity_update_backup'] = TRUE;
+$settings['state_cache'] = TRUE;
+$settings['migrate_node_migrate_type_classic'] = FALSE;
+
+
+$databases['default']['default'] = array (
+  'database' => 'home_sillamaegymn_edu_ee_db',
+  'username' => 'home_sillamaegymn_edu_ee_user',
+  'password' => 'emIN8BNK4xrC8/1K7bVtyIlWsFke3Y+/',
+  'prefix' => '',
+  'host' => '10.0.4.8',
+  'port' => '3306',
+  'isolation_level' => 'READ COMMITTED',
+  'driver' => 'mysql',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);
+
+
+
---
+++ kullamaa.edu.ee/settings.php
@@ -0,0 +1,72 @@
+<?php
+
+
+
+if (!empty($_SERVER['HTTP_X_FORWARDED_PROTO'])
+  && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https'
+) {
+  $_SERVER['HTTPS'] = 'on';
+}
+
+// And enable Drupal’s built-in reverse-proxy trust:
+$settings['reverse_proxy'] = TRUE;
+// List the CIDR(s) or IP(s) of your ingress/load-balancer.
+$settings['reverse_proxy_addresses'] = [
+  '10.42.0.0/16',  // all Pod IPs (including your Ingress)
+  '127.0.0.1',     // local‐CLI testing if you ever curl from inside the box
+];
+// Trust all the X-Forwarded-* headers:
+$settings['reverse_proxy_trusted_headers'] = 31;
+
+
+$settings['config_sync_directory'] = $app_root . '/config/sync';
+
+
+// —————————————————————————————————————————————
+// Trusted host patterns must come first in settings.php
+// —————————————————————————————————————————————
+$settings['trusted_host_patterns'] = [
+  // Both with- and without- “www”
+  '^www\.kullamaa\.edu\.ee$',
+  '^kullamaa\.edu\.ee$',
+];
+
+$base_url = 'https://kullamaa.edu.ee';
+
+$settings['hash_salt']  = file_get_contents(__DIR__ . '/salt.txt');
+
+$settings['update_free_access'] = FALSE;
+$settings['file_public_path'] = 'sites/kullamaa.edu.ee/files';
+$settings['file_private_path'] = 'sites/kullamaa.edu.ee/privatefiles';
+$settings['file_temp_path'] = 'sites/kullamaa.edu.ee/tmp';
+
+/**
+ * OVERRIDES
+ */
+$settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
+
+$settings['file_scan_ignore_directories'] = [
+  'node_modules',
+  'bower_components',
+];
+$settings['entity_update_batch_size'] = 50;
+$settings['entity_update_backup'] = TRUE;
+$settings['state_cache'] = TRUE;
+$settings['migrate_node_migrate_type_classic'] = FALSE;
+
+
+$databases['default']['default'] = array (
+  'database' => 'kullamaa_edu_ee_db',
+  'username' => 'kullamaa_edu_ee_user',
+  'password' => 'ZtiFCByu7mZjykI47zATWQK0MLyJE7uh',
+  'prefix' => '',
+  'host' => '10.0.4.8',
+  'port' => '3306',
+  'isolation_level' => 'READ COMMITTED',
+  'driver' => 'mysql',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);
+
+
+
